5999f69f0a84135bc7c132470d79d8cf5ab2287a
Aryan Rajak <rajakaryan956@gmail.com>
2025-12-11 21:53:54 +0530

    delete a node after a given node

---
+++ SINGLY LINKED LIST/Delete_after.cpp
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<iostream>
+#define null 0
+using namespace std;
+
+//Structure of a singly linked list that contain one data field and one next field that contain address of next node.
+ struct node
+{
+  int data;
+    node *next;
+};
+
+//Declaring some structure type pointer
+node *first,*temp,*ttemp,*p,*q,*r,*ptemp,*pttemp,
+*ntemp,*nttemp,*prev,*current,*next;
+
+//Initializing some pointer with null
+void init()
+{
+ first=temp=ttemp=null;
+}
+
+
+//Creating first node 
+void createfirst(int val)
+{
+ first=new node;
+  first->data=val;
+  first->next=null;
+}
+
+//Adding nod eto linked list
+void addnode(int val)
+{
+  temp=first;
+  while(temp->next!=null)
+  {
+  temp=temp->next;
+
+  }
+  ttemp=new node;
+  ttemp->data=val;
+  ttemp->next=null;
+  temp->next=ttemp;
+}
+
+
+//dispalying the linked list
+void disp()
+{
+ temp=first;
+ while(temp!=null)
+ {
+   cout<<temp->data<<endl;
+   temp=temp->next;
+ }
+}
+
+//DELETING THE NODE AFTER A GIVEN DATA
+void del_after(int x)
+{
+	temp= first;
+	while(temp->data!=x)
+	temp=temp->next;
+	ttemp=temp->next;
+	p= ttemp->next;
+	temp->next=p;
+	ttemp->next=null;
+	delete ttemp;
+}
+
+
+//Main function
+int main(){
+    init();
+    createfirst(10);
+    addnode(20);
+    addnode(30);
+    addnode(40);
+    addnode(50);
+    del_after(30);
+    disp();
+}
