7a00ffd1373a4de071343cdfb102c8a9f9933467
Aryan Rajak <rajakaryan956@gmail.com>
2025-12-21 23:20:52 +0530

    Adding a node before the first node

---
+++ DOUBLY LINKED LIST/Add_Before_First.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <iostream>
+#define null 0
+using namespace std;
+
+// Structure of a doubly linked list that contain one data field, one previous field that contain address of previous node, and one next field that contain address of next node.
+struct dll
+{
+    int data;
+    dll *prev, *next;
+};
+// Declaring some structure type pointer
+dll *first, *temp, *ttemp, *p, *q, *r, *ptemp, *pttemp,
+    *ntemp, *nttemp, *prev, *current, *next;
+
+// Initializing some pointer with null
+void init()
+{
+    first = temp = ttemp = null;
+}
+
+// Creating first node
+void createfirst(int val)
+{
+    first = new dll;
+    first->data = val;
+    first->next = null;
+    first->prev = null;
+}
+
+// dispalying the linked list
+void disp()
+{
+ temp=first;
+ while(temp!=null)
+ {
+   cout<<temp->data<<endl;
+   temp=temp->next;
+ }
+}
+
+//Function to add nodes
+void addnode(int val)
+{
+  temp=first;
+  while(temp->next!=null)
+  {
+  temp=temp->next;
+  }
+  ttemp=new dll;
+  ttemp->data=val;
+  ttemp->next=null;
+  ttemp->prev=temp;
+  temp->next=ttemp;
+}
+
+
+//Adding a node before the first node
+void add_before1(int val)
+{
+	temp=new dll;
+	temp->prev=null;
+	temp->data=val;
+	temp->next=first;
+	first->prev=temp;
+	first=temp;
+}
+
+
+// Main function
+int main()
+{
+    init();
+    createfirst(10);
+    addnode(20);
+    addnode(30);
+    addnode(40);
+    addnode(50);
+    addnode(60);
+    add_before1(70);
+    disp();
+}
