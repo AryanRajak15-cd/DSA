333877deb5a6d7ccd8c1494f0c6636e91130c85e
Aryan Rajak <rajakaryan956@gmail.com>
2025-12-28 00:11:24 +0530

    Adding a node after a given node

---
+++ DOUBLY LINKED LIST/Add_after.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <iostream>
+#define null 0
+using namespace std;
+
+// Structure of a doubly linked list that contain one data field, one previous field that contain address of previous node, and one next field that contain address of next node.
+struct dll
+{
+    int data;
+    dll *prev, *next;
+};
+// Declaring some structure type pointer
+dll *first, *temp, *ttemp, *p, *q, *r, *ptemp, *pttemp,
+    *ntemp, *nttemp, *prev, *current, *next;
+
+// Initializing some pointer with null
+void init()
+{
+    first = temp = ttemp = null;
+}
+
+// Creating first node
+void createfirst(int val)
+{
+    first = new dll;
+    first->data = val;
+    first->next = null;
+    first->prev = null;
+}
+
+// dispalying the linked list
+void disp()
+{
+ temp=first;
+ while(temp!=null)
+ {
+   cout<<temp->data<<endl;
+   temp=temp->next;
+ }
+}
+
+//Function to add nodes
+void addnode(int val)
+{
+  temp=first;
+  while(temp->next!=null)
+  {
+  temp=temp->next;
+  }
+  ttemp=new dll;
+  ttemp->data=val;
+  ttemp->next=null;
+  ttemp->prev=temp;
+  temp->next=ttemp;
+}
+
+
+//Function to add a node after a given node
+void add_After(int after,int val)
+{
+    temp=first;
+	while(temp->data!=after)
+	{
+		temp=temp->next;
+		ttemp=temp->next;
+	}
+	p=new dll;
+	p->data=val;
+	p->prev=temp;
+	p->next=ttemp;
+	temp->next=p;
+	ttemp->prev=p;
+}
+
+
+//Main function
+int main()
+{
+    init();
+    createfirst(10);
+    addnode(20);
+    addnode(30);
+    addnode(40);
+    addnode(50);
+    addnode(60);
+    add_After(20,25); // Adding node with value 25 after node with value 20
+    disp(); // Displaying the linked list
+    return 0;
+}
