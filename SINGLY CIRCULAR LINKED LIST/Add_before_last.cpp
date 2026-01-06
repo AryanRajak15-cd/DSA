20e8ac442eb9720ac0191307e218b5e14a6e94a8
Aryan Rajak <rajakaryan956@gmail.com>
2026-01-06 23:51:47 +0530

    Function to add node before last node

---
+++ SINGLY CIRCULAR LINKED LIST/Add_before_last.cpp
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<iostream>
+#define null 0
+using namespace std;
+
+
+/*Structure of a singly circular linked list that contain one data field and one next field that contain address of next node.
+One and only additional thing is that the last node points to the first node.*/
+struct SCLL
+{
+  int data;
+    SCLL *next;
+};
+SCLL *first,*temp,*ttemp,*p,*q,*r;
+
+
+// Initializing some pointer with null
+void init()
+{
+ first=temp=ttemp=null;
+}
+void createfirst(int val)
+{
+ first=new SCLL;
+  first->data=val;
+  first->next=first;
+}
+
+// dispalying the linked list
+void disp()
+{
+ temp=first;
+ do
+ {
+   cout<<temp->data<<endl;
+   temp=temp->next;
+ }while(temp!=first);
+}
+
+// Function to add nodes
+void addnode(int val)
+{
+    temp = first;
+    while (temp->next != first)
+    {
+        temp = temp->next;
+    }
+    ttemp = new SCLL;
+    ttemp->data = val;
+    ttemp->next = first;
+    temp->next = ttemp;
+}
+
+
+// Function to add node before last node
+void add_beforelast(int val)
+{
+	temp=first;
+	while(temp->next!=first)
+	{
+	ttemp=temp;
+	temp=temp->next;
+}
+    p=new SCLL;
+    p->data=val;
+    p->next=temp;
+    ttemp->next=p;
+	
+}
+    
+//Main function
+int main()
+{
+    init();
+    createfirst(10);
+    addnode(20);
+    addnode(30);
+    addnode(40);
+    addnode(50);
+    add_beforelast(70);
+    disp();
+}
